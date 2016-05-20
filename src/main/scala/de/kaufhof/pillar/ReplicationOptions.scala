63e07c7ea6187f8d9dea3a6040e6355edadc547c
Justin Potter <Justin.Potter@weightwatchers.com>
2016-05-20 13:55:44 -0400

    Add qualifications for config params.

--- src/main/scala/de/kaufhof/pillar/ReplicationOptions.scala
+++ src/main/scala/de/kaufhof/pillar/ReplicationOptions.scala
@@ -5,11 +5,13 @@ sealed trait ReplicationStrategy {
 }
 
 final case class SimpleStrategy(replicationFactor: Int = 3) extends ReplicationStrategy {
+  require(replicationFactor > 0)
 
   override def toString: String = s"{'class' : 'SimpleStrategy', 'replication_factor' : $replicationFactor }"
 }
 
 final case class NetworkTopologyStrategy(dataCenters: Seq[CassandraDataCenter]) extends ReplicationStrategy {
+  require(dataCenters.nonEmpty)
 
   override def toString: String = {
     val replicationFacString = dataCenters.map { dc =>
@@ -20,4 +22,6 @@ final case class NetworkTopologyStrategy(dataCenters: Seq[CassandraDataCenter]) 
   }
 }
 
-final case class CassandraDataCenter(name: String, replicationFactor: Int)
+final case class CassandraDataCenter(name: String, replicationFactor: Int){
+  require(replicationFactor > 0 && name.nonEmpty)
+}
